e49de8950abdbf70ef0bb78bff208a4ea69cf084
Rajkumar das <drajkumar420@gmail.com>
2022-07-20 18:53:07 +0600

    product singel page

--- frontend/js/myanimation.js
+++ frontend/js/myanimation.js
@@ -4,7 +4,7 @@ function vccreveal() {
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 100;
+        var elementVisible = 10;
 
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
